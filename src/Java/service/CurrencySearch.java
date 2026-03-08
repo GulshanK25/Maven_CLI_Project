bc4f384d1f3a51502c03ca14160d5e20dad13b7d
Oluwadunsi <108801270+Oluwadunsi@users.noreply.github.com>
2026-03-08 17:24:57 +0100

    empty currency search service file created

---
+++ src/Java/service/CurrencySearch.java
@@ -0,0 +1,10 @@
+package service;
+
+import java.util.List;
+
+
+public class CurrencySearch {
+    public static List<String> search(String query) {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+}
