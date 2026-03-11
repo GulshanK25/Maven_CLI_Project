0be96a2d891590462b38d560930dbe55caf43999
Gulshank25 <gulshan.chhablani@gmail.com>
2026-03-11 13:43:03 +0100

    fixess

--- src/Java/Test/CurrencySearchTest.java
+++ src/Java/Test/CurrencySearchTest.java
@@ -1,5 +1,7 @@
+package Test;
+import org.junit.jupiter.api.Test;
+import service.CurrencySearch;
 
-/*import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
@@ -26,4 +28,3 @@ public class CurrencySearchTest {
                 "No match should return empty list");
     }
 }
-    */
